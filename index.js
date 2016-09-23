e137989b67e56b5b7060c14b5b1d0ca93d1e7413
Jerry Peng <png.jrry@gmail.com>
2016-09-23 17:05:30 -0400

    operational test

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+
+var options = {
+    root: __dirname
+};
+
+app.get('/', function(req,res) {
+	res.sendFile("test.html",options);
+});
+
+http.createServer(app).listen(process.env.PORT || 3000);
