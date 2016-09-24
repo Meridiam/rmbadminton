1500faa1c834cd6337e072994c778ce97b6cb95f
Jerry Peng <png.jrry@gmail.com>
2016-09-23 21:03:59 -0400

    fixed resource file routing issues

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ var options = {
 };
 
 app.get('/', function(req,res) {
-	res.sendFile("test.html",options);
+	res.sendFile("index.html",options);
 });
 
 app.get('/p', function(req,res) {
 	res.send("id is set to " + req.query.id);
+});
+
+app.get('bootstrap/css/bootstrap.min.css', function(req,res) {
+    res.sendFile("bootstrap/css/bootstrap.min.css", options);
+});
+
+app.get('jquery/jquery.min.js', function(req,res) {
+    res.sendFile("jquery/jquery.min.js", options);
+});
+
+app.get('jquery/jquery.min.js', function(req,res) {
+    res.sendFile("jquery/jquery.min.js", options);
 });
 
 http.createServer(app).listen(process.env.PORT || 3000);
