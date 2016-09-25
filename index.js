66e1b8b93bca5945851b98083befce2c862824f4
Jerry Peng <png.jrry@gmail.com>
2016-09-24 23:34:46 -0400

    changed folder structure, added .gif

--- index.js
+++ index.js
@@ -7,35 +7,14 @@ var options = {
 };
 
 app.get('/', function(req,res) {
-	res.sendFile("index.html",options);
+	res.sendFile("/public/index.html",options);
 });
 
 app.get('/p', function(req,res) {
 	res.send("id is set to " + req.query.id);
 });
 
-app.get('/bootstrap/css/bootstrap.min.css', function(req,res) {
-    res.sendFile("bootstrap/css/bootstrap.min.css", options);
-});
-
-app.get('/jquery/jquery.min.js', function(req,res) {
-    res.sendFile("jquery/jquery.min.js", options);
-});
-
-app.get('/bootstrap/js/bootstrap.min.js', function(req,res) {
-    res.sendFile("bootstrap/js/bootstrap.min.js", options);
-});
-
-app.get('/bootstrap/css/custom.css', function(req,res) {
-    res.sendFile("/bootstrap/css/custom.css", options);
-});
-
-app.get('/bootstrap/css/bootstrap.min.css.map', function(req,res) {
-    res.sendFile("bootstrap/css/bootstrap.min.css.map", options);
-});
-
-app.get('/loop.mp4', function(req,res){
-    res.sendFile("loop.mp4", options);
-});
+app.use(express.static('public'));
+app.use(express.static('assets'));
 
 http.createServer(app).listen(process.env.PORT || 3000);
